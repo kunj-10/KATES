627317d8c9328a2d6accf7c349f0d1bb67a722d0
kunj_10 <kunjvgoyal30@gmail.com>
2025-09-28 22:40:19 +0530

    making folder structure

--- kates/cli.py
+++ kates/cli.py
@@ -4,10 +4,14 @@ app = typer.Typer()
 
 
 @app.command()
-def hello(name: str = "World"):
+def hello(name: str):
     """Say hello"""
     typer.echo(f"Hello {name}!")
 
+@app.command()  
+def goodbbye(name: str):
+    """GoodBye"""
+    typer.echo("goodbye")
 
 if __name__ == "__main__":
     app()
